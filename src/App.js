67ed782c1791eafd1fdabe0cf47129e1170ed33c
ali <salehi186@gmail.com>
2017-05-24 01:43:54 +0430

    ee

--- src/App.js
+++ src/App.js
@@ -5,24 +5,20 @@ import 'font-awesome/css/font-awesome.css'
 import DeviceManager from './DeviceManager';
 import Properties from './Properties'
 import MessageWindow from './MessageWindow'
-import {Ribbon, RibbonGroup, RibbonGroupItem, RibbonButton} from "react-bootstrap-ribbon";
-//import jQuery from 'jquery'
 import "bootstrap/dist/css/bootstrap.css";
 global.jQuery = require('jquery');
 require("bootstrap/dist/js/bootstrap");
 import "react-bootstrap-ribbon/dist/react-bootstrap-ribbon.css";
-import {Button} from './UI';
 
 const MenuItem = (props) => {
-  return <RibbonGroupItem colClass="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-    <RibbonButton>
-      <div className="ribbon-icon">
-        <Button onClick={props.click} text={props.text} icon={props.icon}/>
-      </div>
-      <div>{props.text}
-      </div>
-    </RibbonButton>
-  </RibbonGroupItem>
+  return <button type="button" className="btn btn-default btn-lg" onClick={props.click}>
+    <span className={"fa " + props.icon} aria-hidden="true"></span>
+    <p style={{
+      fontSize: "small"
+    }}>
+      {props.text}
+    </p>
+  </button>
 
 }
 
@@ -38,53 +34,50 @@ class App extends Component {
           <li role="presentation">
             <a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Forms</a>
           </li>
+          <li role="presentation">
+            <a href="#imageEdit" aria-controls="imageEdit" role="tab" data-toggle="tab">ویرایش عکس</a>
+          </li>
         </ul>
         <div className="tab-content">
           <div role="tabpanel" className="tab-pane active" id="home">
-            <Ribbon>
-              <div
-                style={{
-                whiteSpace: "wrap",
-                width: "100%"
-              }}>
-
-                <RibbonGroup title="مدیریت تابلو ها" colClass="col-xs-6">
-                  <MenuItem text="refresh" icon="fa-refresh"/>
-                  <MenuItem text="check" icon="fa-plug"/>
-                </RibbonGroup>
-
-                <RibbonGroup title="مدیریت تابلو ها" colClass="col-xs-6">
-
-                  <MenuItem text="Import" icon="fa-cloud-download"/>
-                  <MenuItem text="Add" icon="fa-image"/>
-                  <MenuItem text="Delete" icon=" fa-trash"/>
-                  <MenuItem text="Move Left" icon=" fa-mail-reply"/>
-                  <MenuItem text="Move Right" icon="fa-mail-forward"/>
-                  <MenuItem text="Send Picture" icon="fa-send"/>
-
-                </RibbonGroup>
-
-              </div>
-            </Ribbon>
+            <MenuItem text="refresh" icon="fa-refresh"/>
+            <MenuItem text="check" icon="fa-plug"/>
+            <MenuItem text="Import" icon="fa-cloud-download"/>
+            <MenuItem text="Add" icon="fa-image"/>
+            <MenuItem text="Delete" icon=" fa-trash"/>
+            <MenuItem text="Move Left" icon=" fa-mail-reply"/>
+            <MenuItem text="Move Right" icon="fa-mail-forward"/>
+            <MenuItem text="Send Picture" icon="fa-send"/>
           </div>
           <div role="tabpanel" className="tab-pane" id="profile">
-            <Ribbon>
-              <div
-                style={{
-                whiteSpace: "wrap",
-                width: "100%"
-              }}>
-                <RibbonGroup title="اطلاعات پایه" colClass="col-xs-12">
-                  <MenuItem text="PlayList" icon="fa-book"/>
-                  <MenuItem text="Devices" icon="fa-book"/>
-                  <MenuItem text="Rules" icon="fa-book"/>
-                </RibbonGroup>
-              
+            <MenuItem text="PlayList" icon="fa-book"/>
+            <MenuItem text="Devices" icon="fa-book"/>
+            <MenuItem text="Rules" icon="fa-book"/>
+          </div>
+          <div role="tabpanel" className="tab-pane row" id="imageEdit">
+
+            <div className=" col-xs-1">
+              <div className="input-group">
+                <input type="color" className="form-control"/>
+                <span className="input-group-addon fa fa-pencil"></span>
+
               </div>
-              </Ribbon>
+            </div>
+            <div className=" col-xs-1">
+
+              <div className="input-group">
+                <span className="input-group-addon fa fa-pencil-square"></span>
+                <input type="color" className="form-control" defaultValue="#ffffff"/>
+              </div>
+            </div>
+            <div className="col-xs-6">
+              <MenuItem text="PlayList" icon="fa-book"/>
+              <MenuItem text="Devices" icon="fa-book"/>
+              <MenuItem text="Rules" icon="fa-book"/>
+            </div>
 
           </div>
-          
+
         </div>
         <div className="ContentHolder">
           <div className="MainContainer">
@@ -101,7 +94,7 @@ class App extends Component {
               Temprature: "12"
             }}/>
           </div>
-          
+
         </div>
         <div className="StatusBar">status</div>
       </div>
--- src/MessageWindow.js
+++ src/MessageWindow.js
@@ -1,26 +1,101 @@
 import React, {Component} from 'react';
-import {Button} from './UI';
-import './Messagewindow.css'
+import {fabric} from 'fabric';
+import './Messagewindow.css';
 
 export default class MessageWindow extends Component {
 
+    constructor(props) {
+        super();
+        this.playlist = {
+            id: 12,
+            name: "test",
+            items: [
+                {
+                    img: '{"objects":[{"type":"circle","originX":"left","originY":"top","left":290,"top":1' +
+                            '82.98,"width":319.88,"height":319.88,"fill":"#c8c885","stroke":null,"strokeWidth' +
+                            '":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","stro' +
+                            'keMiterLimit":10,"scaleX":1.23,"scaleY":0.67,"angle":0,"flipX":false,"flipY":fal' +
+                            'se,"opacity":1,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","' +
+                            'fillRule":"nonzero","globalCompositeOperation":"source-over","transformMatrix":n' +
+                            'ull,"skewX":0,"skewY":0,"radius":159.94059386257265,"startAngle":0,"endAngle":6.' +
+                            '283185307179586},{"type":"i-text","originX":"left","originY":"top","left":135,"t' +
+                            'op":91.08,"width":148.96,"height":45.2,"fill":"rgb(0,0,0)","stroke":null,"stroke' +
+                            'Width":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter",' +
+                            '"strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":fals' +
+                            'e,"opacity":1,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","f' +
+                            'illRule":"nonzero","globalCompositeOperation":"source-over","transformMatrix":nu' +
+                            'll,"skewX":0,"skewY":0,"text":"asdfsfsd","fontSize":40,"fontWeight":"normal","fo' +
+                            'ntFamily":"arial","fontStyle":"","lineHeight":1.16,"textDecoration":"","textAlig' +
+                            'n":"left","textBackgroundColor":"","charSpacing":0,"styles":{"0":{"1":{},"2":{},' +
+                            '"3":{},"4":{},"5":{},"6":{},"7":{},"8":{}}}}],"background":""}',
+                    time: 30,
+                    id: 123,
+                    width: 500,
+                    height: 500,
+                    name: "firstImage"
+                }, {
+                    img: '{"objects":[{"type":"rect","left":50,"top":50,"width":20,"height":20,"fill":"gre' +
+                            'en","overlayFill":null,"stroke":null,"strokeWidth":1,"strokeDashArray":null,"sca' +
+                            'leX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"selectable"' +
+                            ':true,"hasControls":true,"hasBorders":true,"hasRotatingPoint":false,"transparent' +
+                            'Corners":true,"perPixelTargetFind":false,"rx":0,"ry":0}],"background":"rgba(0, 0' +
+                            ', 0, 0)"}',
+                    time: 30,
+                    id: 1243,
+                    width: 500,
+                    height: 500,
+                    name: "firstImage"
+                }
+            ]
+        }
+    }
+
     callAction(action) {}
+    componentDidMount() {
+        let convasList = {};
+        this
+            .playlist
+            .items
+            .map((p, idx) => {
+                let c = new fabric.Canvas(document.getElementById(p.id));
+                c.loadFromJSON(p.img);
+                c.selection = false;
+                c.forEachObject(function (o) {
+                    o.selectable = false;
+                    convasList[p.id] = c;
+                });
+            });
+        this.convasList = convasList;
+    }
+    componentWillUnmount() {
+        this
+            .convasList
+            .each((i, p) => {})
+    }
 
     render() {
         return <div className="MessageWindow">
             <div className="deviceInformation">
-                <br/>
-                Khayam VMS222
-                <br/>
-                ON
-                <br/>
-                Every thing is ok
+                {this.playlist.name}
+
             </div>
             <br/>
             <div className="imageContainer">
-                <img src="/images/1.bmp" alt="this is a test"/>
-                <img src="/images/8.bmp" alt="this is a test"/>
-                <img src="/images/16.bmp" alt="this is a test"/>
+                {this.playlist.items.length}
+                {this
+                    .playlist
+                    .items
+                    .map((m, idx) => <div className="itemContainer" key={"convas_id" + idx}>
+                        <div
+                            className="imageItem"
+                            style={{
+                            width: 400,
+                            height: 200
+                        }}>
+                            <canvas id={m.id} width="400" height="200"></canvas>
+                        </div>
+                    </div>)}
+
             </div>
         </div>;
     }
